f79eac10b0945dfb9958b22ac77df4a2541fd2d6
Kevin F Browne <kbrowneatx@gmail.com>
2013-05-01 14:44:34 -0500

    added views and controllers:

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def index
+	@posts = Post.all
+  end
+
+  def show
+	@post = Post.find(params[:id])
+  end
+
+  def new
+	@post = Post.new
+  end
+  
+  def edit
+	@post = Post.find(params[:id])
+  end
+  
+  def create
+	@post = Post.new(params[:post])
+	if @post.save
+		redirect_to @post
+	end
+	render :edit
+  end
+ 
+  def update
+	@post = Post.find(params[:id])
+	if @post.update_attributes(params[:post])
+		redirect_to @post
+	end
+	render :edit
+  end
+  
+  def destroy
+	@post = Post.find(params[:id])
+	if @post.destroy
+		redirect_to posts_path
+	else
+		flash[:error] = "Error deleting post"
+		redirect_to @post
+	end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Railsblog3::Application.routes.draw do
+  resources :posts
+
+  get "welcome/index"
+  get "welcome/about"
+  get "welcome/legal"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ Railsblog3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
